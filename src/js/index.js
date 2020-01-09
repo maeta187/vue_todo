6f5d1ce8001d6fadbad7de36126f1e1ca7116c1c
maeta187 <nexus18789@gmail.com>
2020-01-09 19:41:07 +0900

    Section31終了

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';  // vueモジュールをインポートしている
-// import VueRouter from 'vue-router';
+import VueRouter from 'vue-router'; //node_modulesのvue-routerをインポートしている
 
-// import routes from 'TodoRouterDir/routes';
+import routes from 'TodoRouterDir/routes';  //rotes.jsをインポートしている
 // import routes from 'TodoVuexDir/routes';
 // import store from 'TodoVuexDir/store';
 // import routes from 'VuexSample/routes';
@@ -10,22 +10,24 @@ import Vue from 'vue';  // vueモジュールをインポートしている
 import '../scss/global.scss';　//importするsassファイルの読み込み
 
 // import myApp from './first';
-import myApp from 'TodoDir';  //importするフォルダーを指定している(パスはwebpack.config.babelに書いてある)
+// import myApp from 'TodoDir';  //importするフォルダーを指定している(パスはwebpack.config.babelに書いてある)
 // "index"はデフォルトドキュメントとして認識されるのでファイル名を省略できる
-// import myApp from 'TodoRouterDir';
+import myApp from 'TodoRouterDir'; //TodoRouterDirに代入されたtodoRouterをインポートしている
 // import myApp from 'TodoVuexDir';
 // import myApp from 'VuexSample';
 
-// Vue.use(VueRouter);
-// const router = new VueRouter({
-//   routes,
-//   mode: 'history',
-// });
+Vue.use(VueRouter); //vueRoter(プラグイン)を使用する宣言を行なっている
+// ●Vue.js全体に影響を与えるものがプラグイン()グローバルなプラグイン
+const router = new VueRouter({  //ルーターインスタンスを作成してオプションを渡している
+  routes,   // `routes: routes` の短縮表記
+  mode: 'history',
+});
+//●VueRouterの設定をしているだけ
 
 // Vueインスタンスの作成
 new Vue({
   el: '#app',
-  // router,
+  router,
   // store,
   render: h => h(myApp),
   // myAppと言うHTMLの要素を作っている
